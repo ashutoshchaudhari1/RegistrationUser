679d0591d985aea4da644902368f2ac734af36dd
ashutosh <ashu.4chaudhari@gmail.com>
2021-01-15 18:22:09 +0530

    userRagistration form

---
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+EditText t1,t2,t3,t4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void process(View view) {
+        t1=(EditText)findViewById(R.id.t1);
+        t2=(EditText)findViewById(R.id.t2);
+
+        t3=(EditText)findViewById(R.id.t3);
+
+        t4=(EditText)findViewById(R.id.t4);
+        String roll=t1.getText().toString().trim();
+        String name=t2.getText().toString().trim();
+        String course=t3.getText().toString().trim();
+        String duration=t4.getText().toString().trim();
+        dataholder obj=new dataholder(name,course,duration);
+        FirebaseDatabase db=FirebaseDatabase.getInstance();
+        DatabaseReference node=db.getReference("students");
+        node.child(roll).setValue(obj);
+        t1.setText("");
+        t2.setText("");
+        t3.setText("");
+        t4.setText("");
+        Toast.makeText(getApplicationContext(),"value Inserted",Toast.LENGTH_LONG).show();
+
+
+
+
+
+    }
+}
